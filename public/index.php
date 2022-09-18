6deef917f7caebf6b6f59d3c3908391e933ed847
Reno Angelo D. Banderlipe <radb@newmediastaff.com>
2022-09-18 16:12:37 +0800

    refs #6 this will serve as the root directory and file of the application closes #6

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$database = require './bootstrap.php';
+
+
+$routes = [
+  '' => '../controllers/index.php',
+  'about' => '../controllers/about.php',
+  'contact' => '../controllers/contact.php',
+];
